0f7e78fa426d86c20457a7926af521c19182234b
illidan <berkovskiilia@gmail.com>
2022-02-27 19:11:14 +0300

    Домашние задания к 3 уроку

---
+++ Berkovsky_Ilya_hw_1/task_1_2.py
@@ -0,0 +1,26 @@
+
+
+def list_divided_by_seven(data_list: list):
+    sum_divided_by_seven = 0
+    for num in data_list:
+        sum_number = 0
+        divided_number = num
+        while (num != 0):
+            sum_number += num % 10
+            num //= 10
+            if sum_number % 7 == 0:
+                sum_divided_by_seven += divided_number
+    return sum_divided_by_seven
+
+
+def add_seventeen_to_list_element(data_list: list):
+    for i in range(len(data_list)):
+        data_list[i] += 17
+    return list_divided_by_seven(data_list)
+
+list_of_odd_numbers = [i ** 3 for i in range(1, 1001, 2)]
+
+print(list_divided_by_seven(list_of_odd_numbers))
+print(add_seventeen_to_list_element(list_of_odd_numbers))
+
+
---
+++ Berkovsky_Ilya_hw_1/task_1_3.py
@@ -0,0 +1,10 @@
+
+
+for i in range(1, 101):
+    if i % 100 in (11, 12, 13, 14, 15) or i % 10 in (5, 6, 7, 8, 9, 0):
+        print(f'{i} процентов')
+    elif i % 10 in (2, 3, 4):
+        print(f'{i} процента')
+    else:
+        print(f'{i} процент')
+
